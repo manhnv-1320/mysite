1424e40ea47b64b496ae0a400561be8b8f97628c
= <=>
2023-11-20 14:31:20 +0700

    Generic list and detail views

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from .models import Book, BookInstance, Author
 
@@ -17,3 +18,26 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'catalog/book_list.html'
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+
+        context['page_title'] = 'Books'
+        return context
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    context = {
+        'book': book,
+    }
+
+    return render(request, 'catalog/book_detail.html', context=context)
